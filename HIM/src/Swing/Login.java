ff8e9da98b86e5f50b9ab067d3dd6c03c37d756d
isedorheering <isedor.heering@gmx.de>
2021-11-25 22:30:10 +0100

    Logo

--- HIM/src/Swing/Login.java
+++ HIM/src/Swing/Login.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -36,35 +37,16 @@ public class Login extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	@SuppressWarnings("unlikely-arg-type")
+
 	public Login() {
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 700, 400);
 		getContentPane().setLayout(null);
 
-		JPanel panel_1 = new JPanel();
-		panel_1.setLayout(null);
-		panel_1.setBackground(Color.RED);
-		panel_1.setBounds(0, 0, 120, 361);
-		getContentPane().add(panel_1);
-
-		JPanel tab1 = new JPanel();
-		tab1.setBounds(0, 70, 120, 21);
-		panel_1.add(tab1);
-
-		JLabel lblNewLabel = new JLabel("Login");
-		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 10));
-		tab1.add(lblNewLabel);
-
-		JLabel lblNewLabel_2 = new JLabel("LOGO");
-		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2.setFont(new Font("Arial", Font.BOLD, 15));
-		lblNewLabel_2.setBounds(10, 11, 100, 48);
-		panel_1.add(lblNewLabel_2);
-
 		JPanel panel = new JPanel();
-		panel.setBounds(130, 11, 544, 339);
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(130, 0, 544, 361);
 		getContentPane().add(panel);
 		panel.setLayout(null);
 
@@ -73,7 +55,7 @@ public class Login extends JFrame {
 		textFieldBenutzer.setHorizontalAlignment(SwingConstants.CENTER);
 		textFieldBenutzer.setFont(new Font("Tahoma", Font.ITALIC, 11));
 		textFieldBenutzer.setColumns(10);
-		textFieldBenutzer.setBounds(175, 110, 157, 32);
+		textFieldBenutzer.setBounds(146, 150, 157, 32);
 		panel.add(textFieldBenutzer);
 
 		textFieldPasswort = new JTextField();
@@ -81,7 +63,7 @@ public class Login extends JFrame {
 		textFieldPasswort.setHorizontalAlignment(SwingConstants.CENTER);
 		textFieldPasswort.setFont(new Font("Tahoma", Font.ITALIC, 11));
 		textFieldPasswort.setColumns(10);
-		textFieldPasswort.setBounds(175, 151, 157, 32);
+		textFieldPasswort.setBounds(146, 191, 157, 32);
 		panel.add(textFieldPasswort);
 
 		JButton btnNewButton_1 = new JButton("Login");
@@ -105,7 +87,19 @@ public class Login extends JFrame {
 		btnNewButton_1.setBackground(Color.RED);
 		btnNewButton_1.setBorderPainted(false);
 		btnNewButton_1.setOpaque(true);
-		btnNewButton_1.setBounds(175, 194, 157, 32);
+		btnNewButton_1.setBounds(146, 234, 157, 32);
 		panel.add(btnNewButton_1);
+
+		JLabel lblNewLabel = new JLabel("Icon/logo");
+		lblNewLabel.setIcon(new ImageIcon(Login.class.getResource("/res/ProjLogo3.png")));
+		lblNewLabel.setBounds(124, 32, 199, 107);
+		panel.add(lblNewLabel);
+
+		JPanel panel_1_1 = new JPanel();
+		panel_1_1.setBounds(0, 0, 146, 361);
+		getContentPane().add(panel_1_1);
+		panel_1_1.setLayout(null);
+		panel_1_1.setBackground(Color.RED);
+
 	}
 }
--- HIM/src/Swing/MenuStu.java
+++ HIM/src/Swing/MenuStu.java
@@ -22,8 +22,7 @@ public class MenuStu extends JFrame {
 	private JTextField txtName;
 	private JTextField txtUnternehmen;
 	private JTextField txtZeitraum;
-	private JTextField txtBenutzer;
-	private JTextField txtPasswort;
+	private JTextField txtBeschreibung;
 
 	/**
 	 * Launch the application.
@@ -46,6 +45,8 @@ public class MenuStu extends JFrame {
 	 * Create the frame.
 	 */
 	public MenuStu() {
+		setResizable(false);
+		setTitle("HIM - HFT Intern Manager");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 700, 400);
 		contentPane = new JPanel();
@@ -55,137 +56,136 @@ public class MenuStu extends JFrame {
 		contentPane.setLayout(null);
 
 		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(0, 0, 120, 361);
+		panel_1.setBounds(0, 0, 146, 361);
 		panel_1.setBackground(Color.RED);
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
 
-		JPanel tab1 = new JPanel();
-		tab1.setBounds(0, 70, 120, 21);
-		panel_1.add(tab1);
+		JPanel tab2 = new JPanel();
+		tab2.setBackground(Color.RED);
+		tab2.setBounds(10, 102, 126, 21);
+		panel_1.add(tab2);
 
-		JLabel lblNewLabel = new JLabel("Log-in");
-		lblNewLabel.addMouseListener(new MouseAdapter() {
+		JLabel lblNewLabel_1 = new JLabel("BPS-Formular");
+		lblNewLabel_1.setForeground(Color.WHITE);
+		lblNewLabel_1.setBounds(0, 0, 116, 21);
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				tabbedPane.setSelectedIndex(0);
 			}
 		});
-		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 10));
-		tab1.add(lblNewLabel);
+		tab2.setLayout(null);
+		lblNewLabel_1.setFont(new Font("Arial", Font.BOLD, 11));
+		tab2.add(lblNewLabel_1);
 
-		JPanel tab2 = new JPanel();
-		tab2.setBounds(0, 102, 120, 21);
-		panel_1.add(tab2);
+		JPanel tab2_1 = new JPanel();
+		tab2_1.setBackground(Color.RED);
+		tab2_1.setBounds(10, 134, 126, 21);
+		panel_1.add(tab2_1);
 
-		JLabel lblNewLabel_1 = new JLabel("BPS-Formular");
-		lblNewLabel_1.addMouseListener(new MouseAdapter() {
+		JLabel lblNewLabel_1_1 = new JLabel("Praktikumsverwaltung");
+		lblNewLabel_1_1.setForeground(Color.WHITE);
+		lblNewLabel_1_1.setBounds(0, 0, 141, 18);
+		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_1_1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				tabbedPane.setSelectedIndex(1);
 			}
 		});
-		lblNewLabel_1.setFont(new Font("Arial", Font.BOLD, 10));
-		tab2.add(lblNewLabel_1);
+		tab2_1.setLayout(null);
+		lblNewLabel_1_1.setFont(new Font("Arial", Font.BOLD, 11));
+		tab2_1.add(lblNewLabel_1_1);
 
-		JPanel tab2_1 = new JPanel();
-		tab2_1.setBounds(0, 134, 120, 21);
-		panel_1.add(tab2_1);
+		JPanel tab2_2 = new JPanel();
+		tab2_2.setBackground(Color.RED);
+		tab2_2.setBounds(10, 166, 126, 21);
+		panel_1.add(tab2_2);
 
-		JLabel lblNewLabel_1_1 = new JLabel("Praktikumsverwaltung");
-		lblNewLabel_1_1.addMouseListener(new MouseAdapter() {
+		JLabel lblNewLabel_1_2 = new JLabel("Dokumente");
+		lblNewLabel_1_2.setForeground(Color.WHITE);
+		lblNewLabel_1_2.setBounds(0, 0, 116, 18);
+		lblNewLabel_1_2.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_1_2.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				tabbedPane.setSelectedIndex(2);
 			}
 		});
-		lblNewLabel_1_1.setFont(new Font("Arial", Font.BOLD, 10));
-		tab2_1.add(lblNewLabel_1_1);
+		tab2_2.setLayout(null);
+		lblNewLabel_1_2.setFont(new Font("Arial", Font.BOLD, 11));
+		tab2_2.add(lblNewLabel_1_2);
 
-		JPanel tab2_2 = new JPanel();
-		tab2_2.setBounds(0, 166, 120, 21);
-		panel_1.add(tab2_2);
+		JPanel tab2_2_1 = new JPanel();
+		tab2_2_1.setBackground(Color.RED);
+		tab2_2_1.setBounds(10, 198, 126, 21);
+		panel_1.add(tab2_2_1);
 
-		JLabel lblNewLabel_1_2 = new JLabel("Dokumente");
-		lblNewLabel_1_2.addMouseListener(new MouseAdapter() {
+		JLabel lblNewLabel_1_2_1 = new JLabel("Profil");
+		lblNewLabel_1_2_1.setForeground(Color.WHITE);
+		lblNewLabel_1_2_1.setBackground(Color.RED);
+		lblNewLabel_1_2_1.setBounds(0, 0, 116, 18);
+		lblNewLabel_1_2_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_1_2_1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				tabbedPane.setSelectedIndex(3);
 			}
 		});
-		lblNewLabel_1_2.setFont(new Font("Arial", Font.BOLD, 10));
-		tab2_2.add(lblNewLabel_1_2);
-
-		JPanel tab2_2_1 = new JPanel();
-		tab2_2_1.setBounds(0, 198, 120, 21);
-		panel_1.add(tab2_2_1);
-
-		JLabel lblNewLabel_1_2_1 = new JLabel("Profil");
-		lblNewLabel_1_2_1.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				tabbedPane.setSelectedIndex(4);
-			}
-		});
-		lblNewLabel_1_2_1.setFont(new Font("Arial", Font.BOLD, 10));
+		tab2_2_1.setLayout(null);
+		lblNewLabel_1_2_1.setFont(new Font("Arial", Font.BOLD, 11));
 		tab2_2_1.add(lblNewLabel_1_2_1);
 
-		JLabel lblNewLabel_2 = new JLabel("LOGO");
-		lblNewLabel_2.setFont(new Font("Arial", Font.BOLD, 15));
-		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2.setBounds(10, 11, 100, 48);
-		panel_1.add(lblNewLabel_2);
+		JPanel tab2_2_2 = new JPanel();
+		tab2_2_2.setLayout(null);
+		tab2_2_2.setBackground(Color.RED);
+		tab2_2_2.setBounds(0, 329, 136, 21);
+		panel_1.add(tab2_2_2);
+
+		JLabel lblNewLabel_1_2_2 = new JLabel("Logout");
+		lblNewLabel_1_2_2.setForeground(Color.WHITE);
+		lblNewLabel_1_2_2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				Login log = new Login();
+				log.setVisible(true);
+				dispose();
+
+			}
+		});
+		lblNewLabel_1_2_2.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_1_2_2.setFont(new Font("Arial", Font.BOLD, 11));
+		lblNewLabel_1_2_2.setBounds(10, 0, 116, 18);
+		tab2_2_2.add(lblNewLabel_1_2_2);
 
 		JPanel panel_2 = new JPanel();
-		panel_2.setBackground(Color.BLACK);
+		panel_2.setBackground(Color.WHITE);
 		panel_2.setForeground(Color.WHITE);
-		panel_2.setBounds(0, 0, 684, 50);
+		panel_2.setBounds(0, 0, 684, 27);
 		contentPane.add(panel_2);
 
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(119, 50, 565, 311);
+		tabbedPane.setBounds(145, 0, 539, 361);
 		contentPane.add(tabbedPane);
 
-		JPanel t1 = new JPanel();
-		tabbedPane.addTab("New tab", null, t1, null);
-		t1.setLayout(null);
-
-		txtBenutzer = new JTextField();
-		txtBenutzer.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		txtBenutzer.setHorizontalAlignment(SwingConstants.CENTER);
-		txtBenutzer.setText("Benutzer");
-		txtBenutzer.setBounds(10, 54, 244, 20);
-		t1.add(txtBenutzer);
-		txtBenutzer.setColumns(10);
-
-		txtPasswort = new JTextField();
-		txtPasswort.setFont(new Font("Tahoma", Font.ITALIC, 11));
-		txtPasswort.setHorizontalAlignment(SwingConstants.CENTER);
-		txtPasswort.setText("Passwort");
-		txtPasswort.setColumns(10);
-		txtPasswort.setBounds(10, 85, 244, 20);
-		t1.add(txtPasswort);
-
-		JButton btnNewButton_1 = new JButton("Log In");
-		btnNewButton_1.setBounds(165, 116, 89, 23);
-		t1.add(btnNewButton_1);
-
-		JLabel lblNewLabel_3_3 = new JLabel("Login");
-		lblNewLabel_3_3.setBounds(10, 11, 252, 32);
-		t1.add(lblNewLabel_3_3);
-
 		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(Color.WHITE);
 		tabbedPane.addTab("New tab", null, panel_3, null);
 		panel_3.setLayout(null);
 
 		JLabel lblNewLabel_3 = new JLabel("BPS-Formular Abgabe");
-		lblNewLabel_3.setBounds(10, 11, 252, 32);
+		lblNewLabel_3.setFont(new Font("Arial", Font.BOLD, 12));
+		lblNewLabel_3.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_3.setBounds(10, 0, 252, 32);
 		panel_3.add(lblNewLabel_3);
 
 		txtMatrikelnummer = new JTextField();
 		txtMatrikelnummer.setFont(new Font("Tahoma", Font.ITALIC, 11));
 		txtMatrikelnummer.setText("Matrikelnummer");
-		txtMatrikelnummer.setBounds(10, 43, 341, 20);
+		txtMatrikelnummer.setBounds(35, 51, 341, 20);
 		panel_3.add(txtMatrikelnummer);
 		txtMatrikelnummer.setColumns(10);
 
@@ -193,61 +193,77 @@ public class MenuStu extends JFrame {
 		txtName.setFont(new Font("Tahoma", Font.ITALIC, 11));
 		txtName.setText("Name");
 		txtName.setColumns(10);
-		txtName.setBounds(10, 74, 341, 20);
+		txtName.setBounds(35, 82, 341, 20);
 		panel_3.add(txtName);
 
 		txtUnternehmen = new JTextField();
 		txtUnternehmen.setFont(new Font("Tahoma", Font.ITALIC, 11));
 		txtUnternehmen.setText("Unternehmen");
 		txtUnternehmen.setColumns(10);
-		txtUnternehmen.setBounds(10, 105, 341, 20);
+		txtUnternehmen.setBounds(35, 113, 341, 20);
 		panel_3.add(txtUnternehmen);
 
 		txtZeitraum = new JTextField();
 		txtZeitraum.setFont(new Font("Tahoma", Font.ITALIC, 11));
 		txtZeitraum.setText("Zeitraum");
 		txtZeitraum.setColumns(10);
-		txtZeitraum.setBounds(10, 136, 341, 20);
+		txtZeitraum.setBounds(35, 144, 341, 20);
 		panel_3.add(txtZeitraum);
 
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(262, 167, 89, 23);
+		JButton btnNewButton = new JButton("versenden");
+		btnNewButton.setBounds(273, 274, 103, 23);
 		panel_3.add(btnNewButton);
 
+		txtBeschreibung = new JTextField();
+		txtBeschreibung.setText("Beschreibung");
+		txtBeschreibung.setFont(new Font("Tahoma", Font.ITALIC, 11));
+		txtBeschreibung.setColumns(10);
+		txtBeschreibung.setBounds(35, 175, 341, 88);
+		panel_3.add(txtBeschreibung);
+
 		JPanel panel_4 = new JPanel();
+		panel_4.setBackground(Color.WHITE);
 		tabbedPane.addTab("New tab", null, panel_4, null);
 		panel_4.setLayout(null);
 
 		JButton btnNewButton_2 = new JButton("hochladen");
-		btnNewButton_2.setBounds(10, 154, 89, 23);
+		btnNewButton_2.setBounds(10, 164, 89, 23);
 		panel_4.add(btnNewButton_2);
 
 		JButton btnNewButton_2_1 = new JButton("hochladen");
-		btnNewButton_2_1.setBounds(105, 154, 89, 23);
+		btnNewButton_2_1.setBounds(109, 164, 89, 23);
 		panel_4.add(btnNewButton_2_1);
 
 		JButton btnNewButton_2_2 = new JButton("hochladen");
-		btnNewButton_2_2.setBounds(204, 154, 89, 23);
+		btnNewButton_2_2.setBounds(208, 164, 89, 23);
 		panel_4.add(btnNewButton_2_2);
 
 		JLabel lblNewLabel_3_1 = new JLabel("Praktikumsverwaltung");
-		lblNewLabel_3_1.setBounds(10, 11, 252, 32);
+		lblNewLabel_3_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_3_1.setFont(new Font("Arial", Font.BOLD, 12));
+		lblNewLabel_3_1.setBounds(10, 0, 252, 32);
 		panel_4.add(lblNewLabel_3_1);
 
 		JPanel panel_5 = new JPanel();
+		panel_5.setBackground(Color.WHITE);
 		tabbedPane.addTab("New tab", null, panel_5, null);
 		panel_5.setLayout(null);
 
-		JLabel lblNewLabel_3_2 = new JLabel("Dokumente");
-		lblNewLabel_3_2.setBounds(10, 11, 252, 32);
-		panel_5.add(lblNewLabel_3_2);
+		JLabel lblNewLabel_3_1_1 = new JLabel("Dokumente");
+		lblNewLabel_3_1_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_3_1_1.setFont(new Font("Arial", Font.BOLD, 12));
+		lblNewLabel_3_1_1.setBounds(10, 0, 252, 32);
+		panel_5.add(lblNewLabel_3_1_1);
 
 		JPanel panel_6 = new JPanel();
+		panel_6.setBackground(Color.WHITE);
 		tabbedPane.addTab("New tab", null, panel_6, null);
 		panel_6.setLayout(null);
 
-		JLabel lblNewLabel_3_2_1 = new JLabel("Profil");
-		lblNewLabel_3_2_1.setBounds(10, 11, 252, 32);
-		panel_6.add(lblNewLabel_3_2_1);
+		JLabel lblNewLabel_3_1_2 = new JLabel("Profil");
+		lblNewLabel_3_1_2.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_3_1_2.setFont(new Font("Arial", Font.BOLD, 12));
+		lblNewLabel_3_1_2.setBounds(10, 0, 252, 32);
+		panel_6.add(lblNewLabel_3_1_2);
 	}
 }
