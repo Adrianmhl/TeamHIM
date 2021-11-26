53dfef3d4764f172384e7a4665b5ca5b01dcbbc9
isedorheering <isedor.heering@gmx.de>
2021-11-26 14:38:31 +0100

    Profil - Login

--- HIM/src/Swing/MenuStu.java
+++ HIM/src/Swing/MenuStu.java
@@ -6,6 +6,7 @@ import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,10 +15,6 @@ import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import javax.swing.ImageIcon;
 
 public class MenuStu extends JFrame {
 	private JTabbedPane tabbedPane;
@@ -27,12 +24,12 @@ public class MenuStu extends JFrame {
 	private JTextField txtUnternehmen;
 	private JTextField txtZeitraum;
 	private JTextField txtBeschreibung;
-	private JTextField tf_name;
-	private JTextField tf_vorname;
-	private JTextField tf_mnr;
-	private JTextField tf_email;
-	private JTextField tf_betreuer;
-	private JTextField tf_praxisstelle;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
 
 	/**
 	 * Launch the application.
@@ -170,7 +167,7 @@ public class MenuStu extends JFrame {
 		lblNewLabel_1_2_2.setFont(new Font("Arial", Font.BOLD, 11));
 		lblNewLabel_1_2_2.setBounds(10, 0, 116, 18);
 		tab2_2_2.add(lblNewLabel_1_2_2);
-		
+
 		JLabel lblNewLabel_5 = new JLabel("New label");
 		lblNewLabel_5.setIcon(new ImageIcon(MenuStu.class.getResource("/res/ProjLogo3.png")));
 		lblNewLabel_5.setBounds(10, 27, 130, 71);
@@ -183,7 +180,7 @@ public class MenuStu extends JFrame {
 		contentPane.add(panel_2);
 
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(145, 64, 539, 297);
+		tabbedPane.setBounds(145, 0, 539, 361);
 		contentPane.add(tabbedPane);
 
 		JPanel panel_3 = new JPanel();
@@ -278,66 +275,66 @@ public class MenuStu extends JFrame {
 		JPanel panel_6 = new JPanel();
 		panel_6.setBackground(Color.WHITE);
 		tabbedPane.addTab("New tab", null, panel_6, null);
-						panel_6.setLayout(null);
-				
-						JLabel lblNewLabel_3_1_2 = new JLabel("Profil");
-						lblNewLabel_3_1_2.setBounds(0, 0, 518, 22);
-						lblNewLabel_3_1_2.setHorizontalAlignment(SwingConstants.LEFT);
-						lblNewLabel_3_1_2.setFont(new Font("Arial", Font.BOLD, 12));
-						panel_6.add(lblNewLabel_3_1_2);
-				
-				JLabel lblNewLabel = new JLabel("Name");
-				lblNewLabel.setBounds(0, 27, 36, 16);
-				panel_6.add(lblNewLabel);
-				
-				JLabel lblVorname = new JLabel("Vorname");
-				lblVorname.setBounds(0, 48, 55, 16);
-				panel_6.add(lblVorname);
-				
-				JLabel lblUnternehmer = new JLabel("Matrikelnummer");
-				lblUnternehmer.setBounds(0, 69, 103, 16);
-				panel_6.add(lblUnternehmer);
-				
-				JLabel lblEmail = new JLabel("Email");
-				lblEmail.setBounds(0, 90, 34, 16);
-				panel_6.add(lblEmail);
-				
-				JLabel lblUnternahmer = new JLabel("Praxisstelle");
-				lblUnternahmer.setBounds(0, 111, 72, 16);
-				panel_6.add(lblUnternahmer);
-		
+		panel_6.setLayout(null);
+
+		JLabel lblNewLabel = new JLabel("Name");
+		lblNewLabel.setBounds(50, 51, 36, 16);
+		panel_6.add(lblNewLabel);
+
+		JLabel lblVorname = new JLabel("Vorname");
+		lblVorname.setBounds(50, 78, 55, 16);
+		panel_6.add(lblVorname);
+
+		JLabel lblUnternehmer = new JLabel("Matrikelnummer");
+		lblUnternehmer.setBounds(50, 105, 103, 16);
+		panel_6.add(lblUnternehmer);
+
+		JLabel lblEmail = new JLabel("Email");
+		lblEmail.setBounds(50, 132, 34, 16);
+		panel_6.add(lblEmail);
+
+		JLabel lblUnternahmer = new JLabel("Praxisstelle");
+		lblUnternahmer.setBounds(50, 159, 72, 16);
+		panel_6.add(lblUnternahmer);
+
 		JLabel lblBetreuer = new JLabel("Betreuer");
-		lblBetreuer.setBounds(0, 132, 51, 16);
+		lblBetreuer.setBounds(50, 186, 51, 16);
 		panel_6.add(lblBetreuer);
-		
-		tf_name = new JTextField();
-		tf_name.setBounds(245, 22, 130, 26);
-		panel_6.add(tf_name);
-		tf_name.setColumns(10);
-		
-		tf_vorname = new JTextField();
-		tf_vorname.setBounds(245, 43, 130, 26);
-		panel_6.add(tf_vorname);
-		tf_vorname.setColumns(10);
-		
-		tf_mnr = new JTextField();
-		tf_mnr.setBounds(245, 64, 130, 26);
-		panel_6.add(tf_mnr);
-		tf_mnr.setColumns(10);
-		
-		tf_email = new JTextField();
-		tf_email.setBounds(245, 90, 130, 26);
-		panel_6.add(tf_email);
-		tf_email.setColumns(10);
-		
-		tf_betreuer = new JTextField();
-		tf_betreuer.setBounds(245, 127, 130, 26);
-		panel_6.add(tf_betreuer);
-		tf_betreuer.setColumns(10);
-		
-		tf_praxisstelle = new JTextField();
-		tf_praxisstelle.setBounds(245, 106, 130, 26);
-		panel_6.add(tf_praxisstelle);
-		tf_praxisstelle.setColumns(10);
+
+		JLabel lblNewLabel_3_1_1_1 = new JLabel("Profil");
+		lblNewLabel_3_1_1_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_3_1_1_1.setFont(new Font("Arial", Font.BOLD, 12));
+		lblNewLabel_3_1_1_1.setBounds(10, 0, 252, 32);
+		panel_6.add(lblNewLabel_3_1_1_1);
+
+		textField = new JTextField();
+		textField.setBounds(232, 50, 280, 18);
+		panel_6.add(textField);
+		textField.setColumns(10);
+
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(232, 76, 280, 18);
+		panel_6.add(textField_1);
+
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(232, 103, 280, 18);
+		panel_6.add(textField_2);
+
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(232, 130, 280, 18);
+		panel_6.add(textField_3);
+
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(232, 157, 280, 18);
+		panel_6.add(textField_4);
+
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(232, 186, 280, 18);
+		panel_6.add(textField_5);
 	}
 }
